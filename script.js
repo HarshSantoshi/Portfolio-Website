c0c5f9d156e46809196eaa4e024786f9f2b559d8
Harsh Santoshi <115166906+HarshSantoshi@users.noreply.github.com>
2023-05-24 17:21:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+// Handling the active link
+let section = document.querySelectorAll('section');
+let link = document.querySelectorAll('header nav ul li a');
+let section1 = document.getElementById('home');
+window.onscroll = ()=>{
+    section.forEach(sect=>{
+        let top = window.scrollY;
+        let offset = sect.offsetTop-60;
+        let height = sect.offsetHeight;
+        let id = sect.getAttribute('id');
+
+        if(top>=offset && top<offset+height){
+            link.forEach(links=>{
+                links.classList.remove('active');
+                document.querySelector('header nav ul li a[href*=' + id + ']').classList.add('active');
+            })
+        }
+        else if(top<section1.offsetTop){
+            link.forEach(links=>{
+                links.classList.remove('active');
+               
+            })
+        }
+    })
+}
+
+
+// Scroll reveal
+ScrollReveal({
+    reset:true,
+    distance:"100px",
+    duration:1000,
+    delay:100
+});
+ScrollReveal().reveal('.left-banner>div , .section-heading ,.skill-img' , {origin:'top'})
+ScrollReveal().reveal('.right-banner , .about-content ,.timeline , .project-container '  , {origin:'bottom'})
+ScrollReveal().reveal('.btn-box , .contact-left ,.skill-content'  , {origin:'left'})
+ScrollReveal().reveal('.contact-right , .skill-img ,.about-image'  , {origin:'right'})
+
+
+
+
+
+
+
+function sendEmail(){
+    Email.send({
+        Host : "smtp.gmail.com",
+        Username : "santoshiharsh30@gmail.com",
+        Password : "HARSH!05052002",
+        To : 'santoshiharsh30@gmail.com',
+        From : document.getElementById('email'),
+        Subject : document.getElementById('subject'),
+        Body : document.getElementById('message')
+    }).then(
+      message => alert(message)
+    );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let element = document.getElementsByClassName("bar")
+for (let i =0 ;i<element.length ;i++){
+    let size = element[i].innerHTML;
+    element[i].style.width = size + "%";
+    element[i].innerHTML ="";
+}
